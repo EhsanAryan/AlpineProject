2a3eb4ccf6bdeeb4f70f445b419040e48dc24ff6
Ehsan <ehsan.aryan2001@gmail.com>
2023-03-20 10:34:53 +0330

    The 4th commit.

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addUserBtn = document.getElementById("add-user-btn");
+    const modalBack = document.querySelector(".add-user-modal-back");
+    const modalBox = document.querySelector(".add-user-modal-box");
+
+    addUserBtn.addEventListener("click", () => {
+        console.log(modalBack)
+        modalBack.classList.remove("d-none");
+        setTimeout(() => {
+            modalBack.classList.add("opacity-1");
+            modalBox.classList.add("show");
+        }, 10);
+    });
+
+    modalBack.addEventListener("click", (ev) => {
+        modalBox.classList.remove("show");
+        modalBack.classList.remove("opacity-1");
+        setTimeout(() => {
+            modalBack.classList.add("d-none");
+        }, 250);
+    });
+
+    modalBox.addEventListener("click", (ev) => {
+        ev.stopPropagation();
+    });
+
+});
