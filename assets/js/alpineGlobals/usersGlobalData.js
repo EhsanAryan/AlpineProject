55e2240cd0c757ec06ff813e096f3dd808ec7c27
Ehsan <ehsan.aryan2001@gmail.com>
2023-03-24 11:47:03 +0430

    The 7th commit.

--- assets/js/alpineGlobals/usersGlobalData.js
+++ assets/js/alpineGlobals/usersGlobalData.js
@@ -1,18 +1,60 @@
 document.addEventListener("alpine:init", () => {
     Alpine.data("usersData", () => ({
         users: [],
+        pageUsers: [],
+        isLoading: false,
+        showAddUserModal: false,
+        pagesCount: 1,
+        currentPage: 1,
+        itemsCount: 4,
         getUsers() {
+            this.isLoading = true;
             axios.get("https://jsonplaceholder.typicode.com/users")
             .then((response) => {
-                 if(response.status === 200) {
+                if(response.status === 200) {
                     this.users = response.data;
-                 } else {
+                    this.setPagination();
+                } else {
                     alert(`Error!\nStatus code: ${response.status}`);
-                 }
+                }
             })
             .catch((error) => {
                 alert(error);
-            })
+            });
+            this.isLoading = false;
+        },
+        setPagination() {
+            this.pagesCount = Math.ceil(this.users.length / this.itemsCount);
+            const startIndex = (this.currentPage - 1) * this.itemsCount;
+            const endIndex = this.currentPage * this.itemsCount;
+            this.pageUsers = this.users.slice(startIndex, endIndex);
+        },
+        setItemsCount(ev) {
+            const itemsCnt = Number(ev.target.value);
+            if(itemsCnt < 1) {
+                this.itemsCount = 1;
+            }
+            else if(itemsCnt > 20) {
+                this.itemsCount = 20;
+            } 
+            else {
+                this.itemsCount = itemsCnt;
+            }
+            this.setPagination();
+        },
+        increaseCurrentPage() {
+            this.currentPage = this.currentPage + 1;
+            this.setPagination();
+        },
+        decreaseCurrentPage() {
+            this.currentPage = this.currentPage - 1;
+            this.setPagination();
+        },
+        deleteUser(userId) {
+            console.log(`Delete: ${userId}`);
+        },
+        editUser(userId) {
+            console.log(`Edit: ${userId}`);
         }
     }));
 });
